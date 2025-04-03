7c4be81fe317f18e373059acfe6db966bd8c7b60
Kalkidan_eme <kalkidan.wondwossen@aastustudent.edu.et>
2025-04-03 15:30:02 +0300

    dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,8 @@
+text = {
+    "name": "kalkidan",
+        "age": "20",
+        "department": "mechatronics"
+        }
+print(text["name"]) #output: "kalkidan"
+print(text["age"]) #output: "20"
+print(text["department"]) #output: "mechatronics"
